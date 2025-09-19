cc893c30b9dde8dee919378fd9af5d88d1523cf6
Dan Page <26827986+D-Pagey@users.noreply.github.com>
2025-09-19 10:17:08 +0100

    add commands functionality

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := scanner.Text()
-		cleaned := cleanInput(input)
-		fmt.Printf("Your command was: %s \n", cleaned[0])
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,17 +1,62 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
-func cleanInput(text string) []string {
-	trimmed := strings.Trim(text, " ")
-	words := strings.Split(trimmed, " ")
-	lowered := []string{}
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
 
-	for _, w := range words {
-		lowered = append(lowered, strings.ToLower(w))
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
 	}
+}
 
-	return lowered
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
