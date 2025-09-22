bd6724e0a32fd7110983db8563ffab401c5f5b9e
Dan Page <26827986+D-Pagey@users.noreply.github.com>
2025-09-22 12:05:26 +0100

    finish explore a location functionality

--- command_explore.go
+++ command_explore.go
@@ -8,6 +8,18 @@ func commandExplore(cfg *config, args []string) error {
 	}
 
 	area := args[0]
-	fmt.Println(area)
+
+	fmt.Println("Exploring " + area + "...")
+
+	locationResp, err := cfg.pokeapiClient.GetLocation(area)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Found Pokemon:")
+	for _, pokemon := range locationResp.PokemonEncounters {
+		fmt.Println(" - " + pokemon.Pokemon.Name)
+	}
+
 	return nil
 }
--- internal/pokeapi/location_get.go
+++ internal/pokeapi/location_get.go
@@ -1,1 +1,47 @@
 package pokeapi
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+// GetLocation gets the pokemon inside a location
+func (c *Client) GetLocation(area string) (RespLocation, error) {
+	url := baseURL + "/location-area/" + area
+
+	if val, ok := c.cache.Get(url); ok {
+		cacheVal := RespLocation{}
+		err := json.Unmarshal(val, &cacheVal)
+		if err != nil {
+			return RespLocation{}, err
+		}
+
+		return cacheVal, nil
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return RespLocation{}, nil
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return RespLocation{}, nil
+	}
+	defer resp.Body.Close()
+
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return RespLocation{}, nil
+	}
+
+	c.cache.Add(url, dat)
+
+	locationResp := RespLocation{}
+	err = json.Unmarshal(dat, &locationResp)
+	if err != nil {
+		return RespLocation{}, nil
+	}
+	return locationResp, nil
+}
